7a31740c4ce44b813eb5d626e0a913922b4f92f1
Ali Khan <muhammadali.khan@teamaol.com>
2015-06-22 11:27:30 +0100

    Updates

---
+++ automation/docker/devimage/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ ! -f /.tomcat_admin_created ]; then
+    /create_tomcat_admin_user.sh
+fi
+echo $(grep $(hostname) /etc/hosts | cut -f1) liquidedge.com >> /etc/hosts 
+service nginx start
+/usr/sbin/sshd -D &
+exec ${CATALINA_HOME}/bin/catalina.sh jpda run
