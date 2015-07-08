3e06f7265b95ccd80f025244367a1c1735e87cfa
Ali Khan <muhammadali.khan@teamaol.com>
2015-07-08 16:27:56 +0100

    Updates

--- automation/docker/devimage/run.sh
+++ automation/docker/devimage/run.sh
@@ -8,16 +8,15 @@ echo "$USERNAME  ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
 
 export ORIGPASSWD=$(cat /etc/passwd | grep $USERNAME)
 
-#export ORIG_UID=$(echo $ORIGPASSWD | cut -f3 -d:)
-#export ORIG_GID=$(echo $ORIGPASSWD | cut -f4 -d:)
-#export DEV_GID=${$ORIG_GID:=$ORIG_GID}
 export DEV_UID=${DEV_UID:=$ORIG_UID}
 
 ORIG_HOME=$(echo $ORIGPASSWD | cut -f6 -d:)
 
-#sed -i -e "s/:$ORIG_UID:$ORIG_GID:/:$DEV_UID:$ORIG_GID:/" /etc/passwd
 chown -R ${DEV_UID} ${ORIG_HOME}
-chown -R ${DEV_UID} /tmp/.X11-unix
+usermod -g jenkins $USERNAME
+
+chown -R jenkins /tmp/.X11-unix
+chown -R jenkins /home/jenkins/.Xauthority
 if [ ! -f /.tomcat_admin_created ]; then
     /create_tomcat_admin_user.sh
 fi
